e0595b7304dfb14922b04617ec8ff6b27ec01414
Kaushlendra Pal <kaushlendrap@JLMP7653-2.local>
2023-05-27 13:21:03 +0530

    [Task] Initial commit for Homeopathy app.

---
+++ HomeopathyApp/HomeopathyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeopathyAppApp.swift
+//  HomeopathyApp
+//
+//  Created by Kaushlendra Pal on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeopathyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
